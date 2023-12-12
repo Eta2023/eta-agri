57955c454020fdf076e614cfedec53207042fc9d
Eta2023 <git@etarsd.com>
2023-12-12 23:37:08 +0300

    lama

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nameAr',
+        'nameEng',
+        'note',
+        'Genus_id',
+        
+    ];
+    public function Genuses()
+    {
+        return $this->belongsTo(Genus::class ,'Genus_id'); // Assuming Classeta is the related model
+    }
 }
